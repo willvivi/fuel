67f077cd3a6105a717a9217369b20789f44cbcde
William Nayrole <willnayrole@gmail.com>
2019-12-04 21:00:06 +0000

    Material result table

--- front/src/components/SearchComponents/SearchResults.component.tsx
+++ front/src/components/SearchComponents/SearchResults.component.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { IGasStation, IFuel } from "../../models/GasStation";
-import {
-  Results,
-  GasStation,
-  Price,
-  GasStationName,
-  GasStationPrices,
-  Container,
-  GasStationDetails,
-  GasStationAddress,
-} from "./SearchResults.style";
+import { Container } from "./SearchResults.style";
+
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 interface SearchResultsProps {
   results?: IGasStation[];
@@ -19,32 +17,69 @@ const SearchResults: React.FC<SearchResultsProps> = (
   props: SearchResultsProps
 ) => (
   <Container>
-    <Results>
-      {props.results &&
-        props.results.map((gasStation: IGasStation) => {
-          return (
-            <GasStation key={gasStation.id}>
-              <GasStationDetails>
-                <GasStationName>
-                  {gasStation.nom && gasStation.nom}
-                </GasStationName>
-                <GasStationAddress>
-                  {gasStation.adresse} - {gasStation.cp} {gasStation.ville}
-                </GasStationAddress>
-              </GasStationDetails>
-              <GasStationPrices>
-                {gasStation.prix.map((price: IFuel) => {
-                  return (
-                    <Price key={price.id}>
-                      {price.nom} - {price.valeur}€
-                    </Price>
-                  );
-                })}
-              </GasStationPrices>
-            </GasStation>
-          );
-        })}
-    </Results>
+    <Paper>
+      {props.results && props.results.length > 0 && (
+        <Table>
+          <TableHead>
+            <TableCell>Station</TableCell>
+            <TableCell>Brand</TableCell>
+            <TableCell>Address</TableCell>
+            <TableCell>City</TableCell>
+            <TableCell>Postcode</TableCell>
+            <TableCell>Services</TableCell>
+            <TableCell>E10 (SP95)</TableCell>
+            <TableCell>E5 (SP95)</TableCell>
+            <TableCell>E5 (SP98)</TableCell>
+            <TableCell>E85</TableCell>
+            <TableCell>Diesel</TableCell>
+          </TableHead>
+          <TableBody>
+            {props.results &&
+              props.results.map((gasStation: IGasStation) => (
+                <TableRow key={gasStation.id}>
+                  <TableCell>{gasStation.nom}</TableCell>
+                  <TableCell>{gasStation.marque}</TableCell>
+                  <TableCell>{gasStation.adresse}</TableCell>
+                  <TableCell>{gasStation.ville}</TableCell>
+                  <TableCell>{gasStation.cp}</TableCell>
+                  <TableCell>
+                    <ul>
+                      {gasStation.services.service.map(service => (
+                        <li>{service}</li>
+                      ))}
+                    </ul>
+                  </TableCell>
+                  <TableCell>
+                    {gasStation.prix.map((prix: IFuel) =>
+                      prix.nom === "E10" ? prix.valeur : ""
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {gasStation.prix.map((prix: IFuel) =>
+                      prix.nom === "SP95" ? prix.valeur : ""
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {gasStation.prix.map((prix: IFuel) =>
+                      prix.nom === "SP98" ? prix.valeur : ""
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {gasStation.prix.map((prix: IFuel) =>
+                      prix.nom === "E85" ? prix.valeur : ""
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    {gasStation.prix.map((prix: IFuel) =>
+                      prix.nom === "Gazole" ? prix.valeur : ""
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      )}
+    </Paper>
   </Container>
 );
 
--- front/src/components/SearchComponents/SearchResults.style.ts
+++ front/src/components/SearchComponents/SearchResults.style.ts
@@ -6,33 +6,4 @@ export const Container = styled.div`
   width: 100%;
 `;
 
-export const Results = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 70%;
-`;
-
-export const GasStation = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin: 10px;
-  padding: 10px;
-  border: 1px gray solid;
-`;
-
-export const GasStationDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 300px;
-`;
-export const GasStationName = styled.div`
-  font-weight: bold;
-`;
-export const GasStationAddress = styled.div`
-  font-size: 0.9em;
-`;
-export const GasStationPrices = styled.div`
-  width: 130px;
-`;
-
 export const Price = styled.div``;
