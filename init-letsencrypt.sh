de63cb6a74f6b4b881b061498b2ce4a55e00b376
Ubuntu <willvivi@fuel-vm.q52athgixm5unpeyvb2sfyxgbd.parx.internal.cloudapp.net>
2019-12-07 20:09:30 +0000

    ssl conf

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(fairedelessence.com www.fairedelessence.com)
+domains=(fairedelessence.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="alexandre.comet@outlook.com" # Adding a valid address is strongly recommended
