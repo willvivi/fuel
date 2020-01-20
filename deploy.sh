6dcca565337abdb660b1d90cbcdfc1e6e9cec1cf
William Nayrole <willnayrole@gmail.com>
2020-01-20 14:44:56 +0100

    clean deploy

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,3 @@ printf "\nBuilding front & back\n"
 sudo docker-compose -f docker-compose.prod.yml build
 sudo docker-compose -f docker-compose.prod.yml down
 sudo docker-compose -f docker-compose.prod.yml up --detach
-printf "\nBuilding CRON\n"
-cd cron/
-npm run build
-printf "\nRun CRON\n"
-node dist/index.js
