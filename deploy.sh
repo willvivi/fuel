6bc72198e46324fce8182450509ebdf8a5746967
William Nayrole <willnayrole@gmail.com>
2020-02-24 15:36:19 +0100

    no-cache deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 printf "Pulling latest version\n"
 git pull
 printf "\nBuilding front & back\n"
-sudo docker-compose -f docker-compose.prod.yml build
+sudo docker-compose -f docker-compose.prod.yml build --no-cache
 sudo docker-compose -f docker-compose.prod.yml down
 sudo docker-compose -f docker-compose.prod.yml up --detach
