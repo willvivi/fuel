43995cf64f019ce153341171c96ef2db1373e275
William Nayrole <6492969+willvivi@users.noreply.github.com>
2022-05-17 01:55:25 +0200

    fix(pwa): update service worker

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./index.scss";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { SnackbarProvider } from "notistack";
 import Button from "@mui/material/Button";
 import { createRoot } from "react-dom/client";
@@ -30,4 +30,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
