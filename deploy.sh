37c9f84b46bed380ea2efd3735e022c9326e3e86
William Nayrole <6492969+willvivi@users.noreply.github.com>
2024-10-12 18:18:58 +0200

    chore: script changes (#50)
    * chore: change deploy script to work on aws
    
    * chore: remove auto triggers on image publish

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 printf "Changing context to fuel's folder"
-cd /home/alexandre/fuel/
+cd /home/ec2-user/fuel/
 pwd
-printf "Pulling latest version\n"
-git pull
-printf "\nBuilding front & back\n"
-docker-compose -f docker-compose.prod.yml build
+printf "\nPulling new images\n"
+docker-compose -f docker-compose.prod.yml pull
+printf "\nRebooting containers\n"
 docker-compose -f docker-compose.prod.yml down
 docker-compose -f docker-compose.prod.yml up --detach
